6c6b566074ab75b6c55920ffcffc078db6c577f7
Pranshu Maheshwari <maheshwari.pranshu511@gmail.com>
2018-09-14 23:12:43 +0530

    Added routes and view templates

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,32 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'TEQIP' });
 });
 
- router.get('/commitee' ,function(req,res){
-   res.render('team',{title:'Commitee'});
+ router.get('/commitee', function(req,res){
+   res.render('commitee',{title:'Commitee'});
  });
+
+ router.get('/quality-circle', function(req,res){
+   res.render('quality_circle',{title: 'Quality Circle'});
+ });
+
+ router.get('/reports', function(req,res){
+   res.render('reports',{title: 'Reports'});
+ });
+
+ router.get('/events', function(req,res){
+   res.render('events',{title: 'Events'});
+ });
+
+ router.get('/staff', function(req,res){
+   res.render('staff',{title: 'Staff'});
+ });
+
+ router.get('/contact', function(req,res){
+   res.render('contact',{title: 'Contact'});
+ });
+
+ router.get('/gallery', function(req,res){
+   res.render('gallery',{title: 'Gallery'});
+ });
+
 module.exports = router;
