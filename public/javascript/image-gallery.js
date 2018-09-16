3fc62bc754bf2866ac4fc424935cddedfadd3945
Pranshu Maheshwari <maheshwari.pranshu511@gmail.com>
2018-09-16 19:33:53 +0530

    Added lightbox for image gallery

--- public/javascript/image-gallery.js
+++ public/javascript/image-gallery.js
@@ -2,7 +2,12 @@ $(document).ready(function(){
   // To add the images
   let container = $('#images');
   for(var i=0;i<7;i++){
-    $('#images').append("<div class='row'><div class='col-md-4'><a href='/images/gallery/" + (3*i+1) + ".jpg'><img src='/images/gallery/" + (3*i+1) + ".jpg' class='img-thumbnail' /></a></div><div class='col-md-4'><a href='/images/gallery/" + (3*i+2) + ".jpg'><img src='/images/gallery/" + (3*i+2) + ".jpg' class='img-thumbnail' /></a></div><div class='col-md-4'><a href='/images/gallery/" + (3*i+3) + ".jpg'><img src='/images/gallery/" + (3*i+3) + ".jpg' class='img-thumbnail' /></a></div>");
+    $('#images').append("<div class='row'><div class='col-md-4'><a href='/images/gallery/" + (3*i+1) + ".jpg' data-toggle='lightbox' data-gallery='gallery'><img src='/images/gallery/" + (3*i+1) + ".jpg' class='img-thumbnail' /></a></div><div class='col-md-4'><a href='/images/gallery/" + (3*i+2) + ".jpg' data-toggle='lightbox' data-gallery='gallery'><img src='/images/gallery/" + (3*i+2) + ".jpg' class='img-thumbnail' /></a></div><div class='col-md-4'><a href='/images/gallery/" + (3*i+3) + ".jpg' data-toggle='lightbox' data-gallery='gallery'><img src='/images/gallery/" + (3*i+3) + ".jpg' class='img-thumbnail' /></a></div>");
   }
-  $('#images').append("<div class='row'><div class='col-md-4'><a href='/images/gallery/22.jpg'><img src='/images/gallery/22.jpg' class='img-thumbnail' /></a></div>");
+  $('#images').append("<div class='row'><div class='col-md-4'><a href='/images/gallery/22.jpg' data-toggle='lightbox' data-gallery='gallery'><img src='/images/gallery/22.jpg' class='img-thumbnail' /></a></div>");
 });
+
+$(document).on('click', '[data-toggle="lightbox"]', function(event) {
+                event.preventDefault();
+                $(this).ekkoLightbox();
+            });
